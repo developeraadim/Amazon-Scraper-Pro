7eae62aa205bd59ae42ba0e6abe949d2553a8146
developeraadim <developeraadim@gmail.com>
2026-07-15 21:19:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-def main():
-    print('Amazon Scraper Pro Starter')
+import sys
 
-if __name__=='__main__':
+from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
+
+from ui.main_window import MainWindow
+
+
+def main():
+
+    app = QApplication(sys.argv)
+
+    app.setApplicationName("Amazon Scraper Pro")
+    app.setOrganizationName("DeveloperAadi")
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
     main()
