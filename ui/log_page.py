e44b110a4b92ffc356d6d423fe8679b0d791faf2
developeraadim <developeraadim@gmail.com>
2026-07-15 21:22:22 +0530

    Create log_page.py

---
+++ ui/log_page.py
@@ -0,0 +1,12 @@
+from PySide6.QtWidgets import QWidget,QVBoxLayout,QLabel
+
+
+class LogPage(QWidget):
+
+    def __init__(self):
+
+        super().__init__()
+
+        layout=QVBoxLayout(self)
+
+        layout.addWidget(QLabel("Logs"))
